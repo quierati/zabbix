87e09b720a8cdfb1c12af5bd666077d43d592c68
Julio Quierati <julio.quierati@gmail.com>
2012-12-11 19:16:21 -0200

    alter if to else if

--- apachelog_collect.sh
+++ apachelog_collect.sh
@@ -22,9 +22,9 @@ awk -v host=$HOST '
     BEGIN { error20x=0; error30x=0; error40x=0; error50x=0; } 
      { if (!/nagios/ && !/Zabbix/ && !/Varnish/) 
        if (/" 20[0-9] /) error20x++
-       if (/" 30[0-9] /) error30x++ 
-       if (/" 40[0-9] /) error40x++
-       if (/" 50[0-9] /) error50x++
+       else if (/- 30[0-9] /) error30x++ 
+       else if (/- 40[0-9] /) error40x++
+       else if (/- 50[0-9] /) error50x++
      }  
     END { 
        printf ("%s apache_error20x %d\n%s apache_error30x %d\n%s apache_error40x %d\n%s apache_error50x %d\n",host, error20x,host, error30x,host, error40x,host, error50x)
